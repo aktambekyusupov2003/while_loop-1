bf46d1fee5c36bfc5f3bfc5290b22272dad595e2
aktam <aktam36911@gmail.com>
2023-05-08 14:04:29 +0500

    uy ishi

--- bin/while_loop01.dart
+++ bin/while_loop01.dart
@@ -3,3 +3,18 @@
   Return the number of odd numbers up to A.
 */
 void main() {}
+int func(int a) {
+  int y = 0;
+  int x = 0;
+  while (y <= a) {
+    if (x % 2 == 1) {
+      y += 1;
+    }
+    y++;
+  }
+  void main() {
+    print(func(33));
+  }
+
+  return x;
+}
--- bin/while_loop04.dart
+++ bin/while_loop04.dart
@@ -2,4 +2,20 @@
   Create function func with "a " argument
   Return the sum of even numbers up to "a".
 */
-void main() {}
+void main() {
+  int func(int a) {
+    int y = 0;
+    int x = 0;
+    while (x <= a) {
+      if (x % 2 == 0) {
+        y += x;
+      }
+      x++;
+    }
+    return y;
+  }
+
+  void main() {
+    print(func(40));
+  }
+}
